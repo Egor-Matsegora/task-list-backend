8740181db6460d5065f2de8a73802a4d866e2fde
Egor Matsegora <yegor.matsegora@gmail.com>
2020-04-20 13:41:19 +0300

    add tasks stat

--- controllers/statistics.controller.js
+++ controllers/statistics.controller.js
@@ -14,9 +14,17 @@ module.exports.getUserStat = async function (req, res) {
     const notesStat = getNotesStat(allNotes);
     const allNotesNumber = allNotes.length;
 
+    const allTasks = await Task.find({
+      user: req.user.id,
+    });
+
+    const tasksStat = getTasksStat(allTasks);
+    const allTasksNumber = allTasks.length;
+    const allDoneTasksNumber = allTasks.filter((task) => task.done).length;
+
     const result = {
       notes: { notesStat, allNotesNumber },
-      tasks: {},
+      tasks: { tasksStat, allTasksNumber, allDoneTasksNumber },
     };
     res.status(200).json(result);
   } catch (error) {
@@ -43,4 +51,27 @@ function getNotesStat(notes = []) {
   });
 
   return daysNotes;
+}
+
+function getTasksStat(tasks = []) {
+  const daysTasks = [];
+
+  tasks.forEach((task) => {
+    const date = moment(task.date).format(dateFormat);
+    const dayStat = daysTasks.find((item) => item.date === date);
+    if (!dayStat) {
+      const stat = {
+        date: date,
+        tasksNumber: 1,
+        doneTasksNumber: 0,
+      };
+      daysTasks.push(stat);
+    } else {
+      const index = daysTasks.indexOf(dayStat);
+      index !== -1 && daysTasks[index].tasksNumber++;
+      index !== -1 && task.done && daysTasks[index].doneTasksNumber++;
+    }
+  });
+  console.log(daysTasks);
+  return daysTasks;
 }
