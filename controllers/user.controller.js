54a1296678af648a4916824e51bb317127a6ab82
Egor Matsegora <yegor.matsegora@gmail.com>
2020-04-29 12:09:12 +0300

    add route and controller for user password vakidation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/User.model');
 const errorHandler = require('../helpers/error-handler.helper');
 
@@ -8,7 +9,22 @@ module.exports.getUserByEmail = async function (req, res) {
     if (candidate) {
       res.status(200).json({ success: true });
     } else {
-      res.status(404).json({ success: false });
+      res.status(200).json({ success: false });
+    }
+  } catch (error) {
+    errorHandler(res, error);
+  }
+};
+
+module.exports.getUserPassword = async function (req, res) {
+  try {
+    const candidate = await User.findOne({ email: req.user.email });
+    const passwordResult = bcrypt.compareSync(req.body.password, candidate.password);
+
+    if (passwordResult) {
+      res.status(200).json({ success: true });
+    } else {
+      res.status(200).json({ success: false });
     }
   } catch (error) {
     errorHandler(res, error);
@@ -17,7 +33,6 @@ module.exports.getUserByEmail = async function (req, res) {
 
 module.exports.getUserInfo = function (req, res) {
   const user = req.user;
-  console.log(user);
   if (Object.keys(user).length) {
     res.status(200).json(user);
   } else {
--- routes/user.route.js
+++ routes/user.route.js
@@ -4,6 +4,7 @@ const controller = require('../controllers/user.controller');
 const router = express.Router();
 
 router.post('/getuser', controller.getUserByEmail);
+router.post('/getuserpassword', passport.authenticate('jwt', { session: false }), controller.getUserPassword);
 router.get('/getuserinfo', passport.authenticate('jwt', { session: false }), controller.getUserInfo);
 
 module.exports = router;
